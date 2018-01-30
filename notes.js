5d88d63c5918e0adacbf0e08be02f9b3093c2ed1
Zayne <berndt.zayne@gmail.com>
2018-01-30 14:37:23 +0000

    simplified inputs with yargs

--- notes.js
+++ notes.js
@@ -1,1 +1,22 @@
-console.log("starting notes.js");
+var addNote = (title, body) => {
+  console.log('Adding Note', title, body);
+};
+
+var getAll = () => {
+  console.log('Getting all  notes');
+};
+
+var getNote = (title) => {
+    console.log('Getting notes', title);
+};
+
+var removeNote = (title) => {
+  console.log ('Removed note', title);
+};
+
+module.exports = {
+  addNote,
+  getAll,
+  getNote,
+  removeNote
+};
