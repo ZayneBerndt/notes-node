a6f964d1f64b3c2dca6102d85b7d07437adf3a78
Zayne <berndt.zayne@gmail.com>
2018-01-30 10:47:48 +0000

    install npm

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
-console.log('starting app');
+console.log('starting app.js');
 
 const fs = require('fs');
 const os = require('os');
-
-var user = os.userInfo();
-console.log(user);
-fs.appendFile('greetings.txt', `Hello ${user.username}!` );
+const _ = require('lodash');
+const notes = require('./notes.js');
