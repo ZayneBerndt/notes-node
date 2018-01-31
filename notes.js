d7e1e8f01da6eeb38116981115a584a5025fa61b
Zayne <berndt.zayne@gmail.com>
2018-01-31 11:31:48 +0000

    Refactoring for reusability

--- notes.js
+++ notes.js
@@ -1,28 +1,31 @@
 const fs = require('fs');
 
+var fetchNotes = () => {
+  try {
+    var notesString = fs.readFileSync('notes-data.json');
+    return JSON.parse(notesString);
+  } catch(e) {
+    return [];
+  }
+};
+
+var saveNotes = (notes) => {
+  fs.writeFileSync('notes-data.json', JSON.stringify(notes));
+};
+
 var addNote = (title, body) => {
-  var notes = [];
+  var notes = fetchNotes();
   var note = {
     //Inside EF6 we dont have to set property as follows title: Title.
     title,
     body
   };
-
-  try {
-
-    var notesString = fs.readFileSync('notes-data.json');
-    notes = JSON.parse(notesString);
-
-  } catch(e) {
-
-
-  }
-
   var duplicateNotes = notes.filter((note) => note.title === title);
 
   if (duplicateNotes.length === 0 ) {
     notes.push(note);
-    fs.writeFileSync('notes-data.json', JSON.stringify(notes));
+    saveNotes(notes);
+    return note;
   }
 };
 
